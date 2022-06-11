035a36a80318bf87e0f1770526775f2e658b51ac
Shirleen19 <YOUR_EMAIL>
2022-06-11 09:37:01 +0300

    modified

--- app/src/main/java/buildyou/workoutlog/LoginActivity.kt
+++ app/src/main/java/buildyou/workoutlog/LoginActivity.kt
@@ -23,7 +23,6 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
         btnLogin =findViewById(R.id.btnLogin)
         tilEmail =findViewById(R.id.tilEmail)
         tilPassword =findViewById(R.id.tilPassword)
@@ -31,26 +30,32 @@ class LoginActivity : AppCompatActivity() {
         etPassword =findViewById(R.id.etPasswordOne)
         tvSignUpOne =findViewById(R.id.tvSignUpOne)
         imgFit =findViewById(R.id.imgFit)
-
         tvSignUpOne.setOnClickListener {
             val intent= Intent(this,SignUpActivity::class.java)
             startActivity(intent)
         }
         btnLogin.setOnClickListener {
-        validateLogin()
+            val intent=Intent(this,HomeActivity::class.java)
+            startActivity(intent)
         }
 
     }
     fun validateLogin(){
         var email =etEmail.text.toString()
         var password = etPassword.text.toString()
+        var error=false
 
         if (email.isBlank()){
             tilEmail.error = getString(R.string.email_required)
+            error=true
         }
         if (password.isBlank()){
             tilPassword.error = getString(R.string.password_required)
+            error=true
         }
-
+        if (!error){
+            startActivity(Intent(this,HomeActivity::class.java))
+            finish()
+        }
     }
 }
